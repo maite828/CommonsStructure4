b207fd91ce6192a1dfa1baf6b14cfd3f7a71958f
Ivan Cuesta <ivan.cuesta@pragsis.com>
2017-08-02 11:04:55 +0200

    Stop execution if file contains bad rows.

--- Code/commons-spark/src/main/scala/com/carrefour/ingestion/commons/relational/raw/RelationalLoaderJob.scala
+++ Code/commons-spark/src/main/scala/com/carrefour/ingestion/commons/relational/raw/RelationalLoaderJob.scala
@@ -1,7 +1,8 @@
 package com.carrefour.ingestion.commons.relational.raw
 
-import com.carrefour.ingestion.commons.util.SparkJob
+import com.carrefour.ingestion.commons.util.{SparkJob, SqlUtils}
 import com.carrefour.ingestion.commons.util.transform.{FieldInfo, FieldTransformationUtil, TransformationInfo}
+import com.carrefour.ingestion.commons.exceptions.RowFormatException
 import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SQLContext}
@@ -11,14 +12,14 @@ object RelationalLoaderJob extends SparkJob[RelationalLoaderJobSettings] {
 
   val Logger = LoggerFactory.getLogger(getClass)
 
-  override def run(settings: RelationalLoaderJobSettings)(implicit sqlContext: SQLContext): Unit = {
+  override def run(jobSettings: RelationalLoaderJobSettings)(implicit sqlContext: SQLContext): Unit = {
     //Load metadata objects
-    val metadata = IngestionMetadataLoader.loadMetadata(settings)
-    val transformations = FieldTransformationUtil.loadTransformations(settings.transformationsTable)
+    val metadata = IngestionMetadataLoader.loadMetadata(jobSettings)
 
     //Starting files load
     val fs = FileSystem.get(sqlContext.sparkContext.hadoopConfiguration)
-    metadata.foreach( settings => {
+    metadata.foreach(settings => {
+      val transformations = FieldTransformationUtil.loadTransformations(settings.transformationsTable)
       Logger.info(s"Loading file ${settings.inputPath}")
       val path = new Path(settings.inputPath)
       val status = fs.globStatus(path)
@@ -29,14 +30,14 @@ object RelationalLoaderJob extends SparkJob[RelationalLoaderJobSettings] {
       }
 
 
-//      val (inputFiles: Seq[String], singleFile: Boolean) = if (fs.isDirectory(path)) {
-//        val it = fs.listFiles(path, false)
-//        var files: Seq[String] = Seq[String]()
-//        while (it.hasNext()) {
-//          files = it.next().getPath.toString() +: files
-//        }
-//        (files, false)
-//      } else (Seq(path.toString()), true)
+      //      val (inputFiles: Seq[String], singleFile: Boolean) = if (fs.isDirectory(path)) {
+      //        val it = fs.listFiles(path, false)
+      //        var files: Seq[String] = Seq[String]()
+      //        while (it.hasNext()) {
+      //          files = it.next().getPath.toString() +: files
+      //        }
+      //        (files, false)
+      //      } else (Seq(path.toString()), true)
 
       val fullOutputTable = s"${settings.outputDb}.${settings.outputTable}"
 
@@ -45,10 +46,10 @@ object RelationalLoaderJob extends SparkJob[RelationalLoaderJobSettings] {
   }
 
   /**
-   * Loads the file in the given path into a table in the output DB,
-   * applying the given field transformations. Output table name will be given
-   * by the file name and field names by the file header.
-   */
+    * Loads the file in the given path into a table in the output DB,
+    * applying the given field transformations. Output table name will be given
+    * by the file name and field names by the file header.
+    */
   def loadFile(inputPath: String, outputTable: String, transformations: Map[String, Map[String, TransformationInfo]])(implicit settings: RelationalLoaderJobSettings, sqlContext: SQLContext): Unit = {
     val loadYear = settings.year
     val loadMonth = settings.month
@@ -72,67 +73,82 @@ object RelationalLoaderJob extends SparkJob[RelationalLoaderJobSettings] {
 
     val fieldsInfo = extractFieldsInfo(input, outputTable, transformations)
 
+    // Transformation and write
     Logger.info(s"Inserting in table $outputTable with fields: ${fieldsInfo.map(_.field).mkString(",")}")
-
-    val contentRaw = if (settings.header > 0)
-      // discard header  
-      input.mapPartitionsWithIndex { (idx, iter) =>
-      if (idx == 0) {
-        iter.drop(settings.header)
-      } else iter
-    }
-    else input
-
-    val content = (if (settings.format == RelationalFormats.GzFormat) repartition(contentRaw, settings.numPartitions) else contentRaw).
-      // filter empty records
-      filter(!_.isEmpty()).
-      // split fields
-      map(_.split(settings.fieldDelimiter, -1)).
-      // apply transformations
-      map(fields => (fields zip fieldsInfo).
-        map { case (fieldValue, fieldInfo) => fieldInfo.transformation.transform(fieldValue, fieldInfo.transformationArgs: _*) }).
-      // add partition fields
-      map(x => x ++ Array(loadYear, loadMonth, loadDay)).
-      // build row
-      map(Row(_: _*))
-    
-    Logger.info(s"Dropping existing partition: year=${settings.year}, month=${settings.month}, day=${settings.day}") 
-    sqlContext.sql(s"ALTER TABLE $outputTable DROP IF EXISTS PARTITION (year=${settings.year}, month=${settings.month}, day=${settings.day})")
-    Logger.info(s"Writing in table $outputTable")
-//    Logger.info(s"Content First Line:\n ${content.first()}")
-//    Logger.info(s"Table Schema:\n ${schema} ")
-//    Logger.info(s"Dataframe Schema:\n ${df.printSchema()} ")
     try {
+      val contentRaw = if (settings.header > 0)
+      // discard header
+        input.mapPartitionsWithIndex { (idx, iter) =>
+          if (idx == 0) {
+            iter.drop(settings.header)
+          } else iter
+        }
+      else input
+
+      val regs = (if (settings.format == RelationalFormats.GzFormat) repartition(contentRaw, settings.numPartitions) else contentRaw).
+        // filter empty records
+        filter(!_.isEmpty()).
+        // split fields
+        map(_.split(settings.fieldDelimiter, -1))
+
+      // mark bad records and apply transformations
+      val content = regs.
+        map(fields => {
+          if (fields.length == fieldsInfo.length - 3) {
+            val transField = (fields zip fieldsInfo).
+              map {
+                case (fieldValue, fieldInfo) => fieldInfo.transformation.transform(fieldValue, fieldInfo.transformationArgs: _*)
+              }
+            // add partition fields and generate Row
+            Row(transField ++ Array(loadYear, loadMonth, loadDay) :_*)
+          } else {
+            //TODO Continue execution if there is more entities to load
+            throw new RowFormatException(s"Row length error. RDD length = ${fields.length} -- Table Row Length = ${fieldsInfo.length - 3}. Row: ${fields.mkString(";")}")
+          }
+        })
+
+      Logger.info(s"Dropping existing partition: year=${settings.year}, month=${settings.month}, day=${settings.day}")
+      SqlUtils.sql("/hql/dropPartitionYearMonthDay.hql",
+        outputTable,
+        settings.year.toString,
+        settings.month.toString,
+        settings.day.toString)
+
+      Logger.info(s"Writing in table $outputTable")
       val schema = sqlContext.table(outputTable).schema
-      val prueba = content.map(r =>
-          if (r.length != 33)
-            throw new Exception(s"Row errónea: ${r} ")
-      )
-      Logger.info(s"Primera fila del RDD:\n ${prueba.first()}")
       val df = sqlContext.createDataFrame(content, schema)
       df.write.insertInto(outputTable)
     }
-    catch{
+    catch {
       case e: Exception =>
-        Logger.info(s"Primera fila del RDD:\n ${content.first()}")
-        Logger.info(s"${content.first().schema}")
         e.printStackTrace()
         throw e
     }
   }
 
   /**
-   * Builds the {@link FieldInfo} sequence from the given input RDD and field transformations specification.
-   * Field names are taken from Hive Table Definition.
-   */
+    * Builds the {@link FieldInfo} sequence from the given input RDD and field transformations specification.
+    * Field names are taken from Hive Table Definition.
+    */
   def extractFieldsInfo(input: RDD[String], tableName: String, transformations: Map[String, Map[String, TransformationInfo]])(implicit settings: RelationalLoaderJobSettings, sqlContext: SQLContext): Seq[FieldInfo] = {
     val fieldNames = sqlContext.table(tableName).schema.fields.map(_.name)
     FieldInfo.buildFieldsInfo(tableName.split("\\.").last, fieldNames, transformations)
   }
 
   /**
-   * Repartitions the RDD if numPartitions > 0
-   */
+    * Repartitions the RDD if numPartitions > 0
+    */
   def repartition[T](rdd: RDD[T], partitions: Int): RDD[T] = if (partitions > 0) rdd.repartition(partitions) else rdd
+
+  /**
+    * Validate RDD row format
+    */
+  def validateRowLength[T](row: Row, num_fields:Int): Boolean = {
+    if (row.length == num_fields)
+      true
+    else
+      false
+  }
+
 
 }
