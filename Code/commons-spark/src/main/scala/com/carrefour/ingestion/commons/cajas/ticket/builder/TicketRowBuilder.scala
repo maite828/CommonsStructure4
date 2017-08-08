e14b46af22486e5927218556eeb1a2a9a3c6c71d
Daniel Hierro <daniel.hierro@pragsis.com>
2017-08-08 13:29:51 +0200

    Añadidos campos de particionamiento (year,month,day) y configuracion de reparticionamiento de RDD tras lectura y DF en escritura.

--- Code/commons-spark/src/main/scala/com/carrefour/ingestion/commons/cajas/ticket/builder/TicketRowBuilder.scala
+++ Code/commons-spark/src/main/scala/com/carrefour/ingestion/commons/cajas/ticket/builder/TicketRowBuilder.scala
@@ -2,7 +2,7 @@ package com.carrefour.ingestion.commons.cajas.ticket.builder
 
 import com.carrefour.ingestion.commons.cajas.ticket.TicketInfo
 import com.carrefour.ingestion.commons.util.transform.{FieldInfo, TransformationInfo}
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.types.StructType
 import org.slf4j.LoggerFactory
 
@@ -14,22 +14,25 @@ abstract class TicketRowBuilder(allTransformations: Map[String, Map[String, Tran
   /**
    * Transformations to be applied to each fields, preserving field order
    */
-  lazy val fieldsInfo: Seq[FieldInfo] = FieldInfo.buildFieldsInfo(tableName, TicketRowBuilder.CommonFields ++ fieldNames, allTransformations)
+  lazy val fieldsInfo: Seq[FieldInfo] = FieldInfo.buildFieldsInfo(tableName, TicketRowBuilder.CommonFields ++ fieldNames ++ Array("year","month","day"), allTransformations)
 
   def tableName: String
 
   final def getSchema: StructType = StructType(fieldsInfo.map(_.schema))
 
+  final def getSchema(database: String, table: String)(sqlContext: SQLContext): StructType = sqlContext.table(s"${database}.${table}").schema
+
   final def buildRow(ticketInfo: TicketInfo, fields: Seq[String]): Row = {
-    var rawFields = Array(ticketInfo.fecha, ticketInfo.fecha, ticketInfo.tienda, ticketInfo.pos, ticketInfo.num) ++ selectFields(fields, ticketInfo)
-    if (rawFields.size < fieldsInfo.size) {
+    var rawFields = Array(ticketInfo.fecha, ticketInfo.tienda, ticketInfo.pos, ticketInfo.num) ++ selectFields(fields, ticketInfo)
+    //FIXME checking the numbers of fields is correct
+    if (rawFields.size < fieldsInfo.size - 3) {
       //FIXME use log (nullpointerexception in distributed mode)
       Logger.warn(s"Expected ${fieldsInfo.size} fields, found ${rawFields.size}: ${rawFields.mkString("||")}. Filling missing fields with nulls.")
       rawFields = rawFields ++ Array.fill(fieldsInfo.size - rawFields.size)(null)
     }
     val transformedFields = (rawFields zip fieldsInfo).
       map { case (fieldValue, fieldInfo) => fieldInfo.transformation.transform(fieldValue, fieldInfo.transformationArgs: _*) }
-    Row(transformedFields: _*)
+    Row(transformedFields ++ Array((ticketInfo.year), (ticketInfo.month), (ticketInfo.day)): _*)
   }
 
   /**
@@ -44,14 +47,12 @@ abstract class TicketRowBuilder(allTransformations: Map[String, Map[String, Tran
 }
 
 object TicketRowBuilder {
-  val DatePartField = "date_part"
   val FechaField = "fecha"
   val TiendaField = "tienda"
   val PosField = "pos"
   val NumTicketField = "numtk"
 
   private val CommonFields = Array(
-    DatePartField,
     FechaField,
     TiendaField,
     PosField,
