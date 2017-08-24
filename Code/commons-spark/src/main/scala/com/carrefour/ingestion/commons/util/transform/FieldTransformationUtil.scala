5c41f4124acc8b30f1a381cce4a27df2fbc44ea0
Daniel Hierro <daniel.hierro@pragsis.com>
2017-08-24 14:52:49 +0200

    Fixed data checking when the input data is null or empty.

--- Code/commons-spark/src/main/scala/com/carrefour/ingestion/commons/util/transform/FieldTransformationUtil.scala
+++ Code/commons-spark/src/main/scala/com/carrefour/ingestion/commons/util/transform/FieldTransformationUtil.scala
@@ -34,31 +34,38 @@ object FieldTransformationUtil {
   def applySchema(fields: Seq[Any], structType: StructType, settings: IngestionMetadata): Seq[Any] = {
 
     if (fields.size != structType.size) {
-      throw new RowFormatException("Error when applying type conversion to data. Number of input fields does not match the structure loaded from the table.")
+      throw new RowFormatException(
+        s"""Error when applying type conversion to data. Number of input fields does not match the structure loaded from the table.
+          |Schema loaded: $structType
+          |Fields received: $fields
+        """.stripMargin)
     } else {
     val f = fields zip structType.fields
     val converted = f.map(x => {
       val cleaned = x._1 match {
-        case f: String => cleanField(f)(settings)
+        case f: String => cleanField(f)(settings).getOrElse("")
         case other => other
           
       }
         
         (cleaned, x._2.dataType) match {
           case (field: String, DoubleType) =>
-            if (field == null) null else field.toDouble
+            if (field.isEmpty) null else field.toDouble
           case (field: String, IntegerType) =>
-            if (field == null) null else field.toInt
+            if (field.isEmpty) null else field.toInt
           case (field: String, LongType) =>
-            if (field == null) null else field.toLong
+            if (field.isEmpty) null else field.toLong
           case (field: String, ShortType) =>
-            if (field == null) null else field.toShort
+            if (field.isEmpty) null else field.toShort
           case (field: String, BooleanType) =>
-            if (field == null) null else field.toBoolean
+            if (field.isEmpty) null else field.toBoolean
           case (field: String, DecimalType()) =>
-            if (field == null) null else new java.math.BigDecimal(field)
+            if (field.isEmpty) {
+              val dec:java.math.BigDecimal = null
+              dec
+            } else new java.math.BigDecimal(field)
           case (field: String, TimestampType) =>
-            if (field == null) null else {
+            if (field.isEmpty) null else {
               val format = new java.text.SimpleDateFormat(settings.timestampFormat)
               new Timestamp(format.parse(field).getTime)
             }
@@ -73,9 +80,10 @@ object FieldTransformationUtil {
     }
   }
 
-  def cleanField(field: String)(settings: IngestionMetadata): String = {
-    if (field.isEmpty || field.equalsIgnoreCase("null")) null
-    else field.trim.replace(settings.encloseChar, "")
+  def cleanField(field: String)(settings: IngestionMetadata): Option[String] = {
+    val cleaned: String = field.replace(settings.encloseChar, "").trim
+    if (cleaned.isEmpty || cleaned.equalsIgnoreCase("null")) None
+    else Some(cleaned)
   }
 }
 
