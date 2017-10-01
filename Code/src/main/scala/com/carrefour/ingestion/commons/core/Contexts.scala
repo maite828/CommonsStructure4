24ae9fc9408160a7d5e0625afdd1d22109fe81e2
Maite Echeverry <echeverry.maite@gmail.com>
2017-10-02 01:17:10 +0200

    ACTUALIZADO 02 octubre 2017

--- Code/src/main/scala/com/carrefour/ingestion/commons/core/Contexts.scala
+++ Code/src/main/scala/com/carrefour/ingestion/commons/core/Contexts.scala
@@ -1,16 +1,14 @@
 package com.carrefour.ingestion.commons.core
 
-import com.carrefour.ingestion.commons.context.SparkSessionContext
-import com.carrefour.ingestion.commons.context.impl.SparkSessionContextImpl
-import com.carrefour.ingestion.commons.repository.FileSystemRepository
-import com.carrefour.ingestion.commons.repository.impl.FileSystemRepositoryImpl
+import com.carrefour.ingestion.commons.context.impl.{FileSystemContextImpl, SparkSessionContextImpl}
+import com.carrefour.ingestion.commons.context.{FileSystemContext, SparkSessionContext}
 
 /**
   * In principle private with the idea of ​​being able to delegate only in the methods that are decided
   */
 object Contexts {
 
-  val dfs: FileSystemRepository = FileSystemRepositoryImpl
+  val dfs: FileSystemContext = FileSystemContextImpl
   val spark: SparkSessionContext = SparkSessionContextImpl
 }
 
