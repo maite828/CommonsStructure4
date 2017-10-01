28d710e3f11a56f04959337c1aae33edf9c6c527
Maite Echeverry <echeverry.maite@gmail.com>
2017-10-02 01:20:40 +0200

    ACTUALIZADO 02 octubre 2017

--- Code/src/main/scala/com/carrefour/ingestion/commons/core/Repositories.scala
+++ Code/src/main/scala/com/carrefour/ingestion/commons/core/Repositories.scala
@@ -1,9 +1,7 @@
 package com.carrefour.ingestion.commons.core
 
-import com.carrefour.ingestion.commons.context.SparkSessionContext
-import com.carrefour.ingestion.commons.context.impl.SparkSessionContextImpl
-import com.carrefour.ingestion.commons.repository.{FileSystemRepository, HiveRepository}
-import com.carrefour.ingestion.commons.repository.impl.{FileSystemRepositoryImpl, HiveRepositoryImpl}
+import com.carrefour.ingestion.commons.repository.impl.{FileSystemRepositoryImpl, HiveRepositoryImpl, SparkSessionRepositoryImpl}
+import com.carrefour.ingestion.commons.repository.{FileSystemRepository, HiveRepository, SparkSessionRepository}
 
 /**
   * In principle private with the idea of ​​being able to delegate only in the methods that are decided
@@ -12,6 +10,6 @@ object Repositories {
 
   val hive: HiveRepository = HiveRepositoryImpl
   val dfs: FileSystemRepository = FileSystemRepositoryImpl
-  val spark: SparkSessionContext = SparkSessionContextImpl
+  val spark: SparkSessionRepository = SparkSessionRepositoryImpl
 
 }
